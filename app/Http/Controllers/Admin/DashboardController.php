128d0ac599bf4ea6f3652686f3dc293b599e5241
AMR-Re <amrr8020@gmail.com>
2024-05-22 02:45:39 +0300

    Admin Profile Page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $data['getUser']=User::getUser(Auth::user()->id);
+        return view('admin.dashboard',$data);
     }
 }
